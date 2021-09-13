384b79947dcb88340d75128a2f033447f32e3b29
trangntt-016 <trangntt.016@gmail.com>
2021-09-13 18:30:23 -0400

    add feature 5

--- src/main/java/com/os/jssg/processor/HTMLProcessor.java
+++ src/main/java/com/os/jssg/processor/HTMLProcessor.java
@@ -7,10 +7,13 @@ import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 
@@ -43,15 +46,19 @@ public class HTMLProcessor {
             else {
                 // reset dist and create one if not exists
                 FileUtils.resetDist();
+                List<String>HTMLFileNames = new ArrayList<>();
 
                 Files.list(filePath).forEach(f->{
                     Map htmlMap = HTMLUtils.convertTextToHTML(f.toAbsolutePath().toString());
+                    HTMLFileNames.add(htmlMap.get("title").toString());
                     try {
                         FileUtils.createHTMLFile(htmlMap, outputPath);
+
                     } catch (IOException e) {
                         logger.error(e.getMessage());
                     }
                 });
+                FileUtils.createIndexHTML(HTMLFileNames, outputPath);
             }
         }
         catch(IOException ex){
--- src/main/java/com/os/jssg/utils/FileUtils.java
+++ src/main/java/com/os/jssg/utils/FileUtils.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -55,6 +56,46 @@ public class FileUtils {
         else{
             Files.createDirectories(distPath);
         }
+    }
+
+    public static void createIndexHTML(List<String>fileNames,  String outputPathStr)throws IOException{
+        Path outputPath = Paths.get(outputPathStr);
+
+        if(!Files.exists(outputPath)){
+            logger.error("Specified output path is not a valid directory");
+            return;
+        }
+
+        // write new html files to dist
+        StringBuilder sb = new StringBuilder();
+        String bodyContent = FileUtils.generateStoriesHTML(fileNames);
+        sb.append("<!doctype html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "  <meta charset=\"utf-8\">\n" +
+                "  <title>Generated Pages</title>\n" +
+                "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
+                "  <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\"\n" +
+                "        integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\"></head>\n" +
+                "<body class=\"container d-flex flex-column justify-content\" style=\"background: #161f27\">\n" +
+                "<h1 style=\"color: white; text-align: center;margin-bottom:30px;\">Generated Pages</h1>");
+        sb.append(bodyContent).append("</body></html>");
+
+        Path path = Paths.get(outputPath.toString() +"\\"+ "index.html");
+
+
+        byte[] strToBytes = sb.toString().getBytes();
+
+        Files.write(path, strToBytes);
+    }
+
+    public static String generateStoriesHTML(List<String>fileNames){
+        StringBuilder sb = new StringBuilder();
+
+        for(String f: fileNames){
+            sb.append("<a href=\"/"+f+".html"+"\"><h4 style=\"color: blue; text-align: center;font-weight:400\">"+f+"</h4></a>");
+        }
+        return sb.toString();
     }
 
 
