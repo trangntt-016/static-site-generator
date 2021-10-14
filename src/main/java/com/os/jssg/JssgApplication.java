01536dc438c1ed444b375f2d9d0020b92aaaafc6
trangntt-016 <trangntt.016@gmail.com>
2021-10-13 21:25:10 -0400

    first step: remove runwithconfig()

--- src/main/java/com/os/jssg/JssgApplication.java
+++ src/main/java/com/os/jssg/JssgApplication.java
@@ -1,27 +1,21 @@
 package com.os.jssg;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonIOException;
-import com.google.gson.JsonSyntaxException;
 import com.google.gson.stream.JsonReader;
 import com.os.jssg.processor.HTMLProcessor;
 
-import org.apache.commons.io.FilenameUtils;
 
 import lombok.SneakyThrows;
 import picocli.CommandLine;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-
-import java.util.List;
 
 
 @CommandLine.Command(name = "jssg",
         description = "A tool that generates a full static HTML website based on raw text",
         version = "JSSG v.1.0",
         header = "Java Static Site Generator")
-class JssgApplication implements Runnable {
+public class JssgApplication implements Runnable {
     @CommandLine.Option(names = {"-v", "--version"}, versionHelp = true, description = "Print version and exit")
     Boolean versionHelpRequested = false;
 
@@ -51,39 +45,26 @@ class JssgApplication implements Runnable {
             new CommandLine(this).printVersionHelp(System.err);
         }
         else if(usageHelpRequested){
-            new CommandLine(this).usage(System.err);          
-        } 
+            new CommandLine(this).usage(System.err);
+        }
         // Check for `--config` flag should always be after `--version` and `--help` flag
         else if(config != null){
-            runWithConfig();
+            // parse JSON
+            Gson gson = new Gson();
+            JssgApplication args = gson.fromJson(new JsonReader(new FileReader(config)), JssgApplication.class);
+
+            // assign values so that later on they can be used in the next if block
+            input = args.input;
+            output = args.output;
+            lang = args.lang;
         }
-        else if(input!=null){
+        if(input!=null){
             HTMLProcessor processor = new HTMLProcessor();
             processor.convertToHTML(input, output, lang);
         }
     }
 
-    // run the application with the passed config file path from CLI
-    public void runWithConfig() {
-        if(config == null) return;
 
-        if(!FilenameUtils.getExtension(config).equals("json")) {
-            System.err.println("Only accept json file");
-            System.exit(1);
-        };
-
-        try {
-            Gson gson = new Gson();
-            JssgApplication args = gson.fromJson(new JsonReader(new FileReader(config)), JssgApplication.class);
-            
-            HTMLProcessor processor = new HTMLProcessor();
-            processor.convertToHTML(args.input, args.output, args.lang);
-        } catch (JsonIOException | JsonSyntaxException | FileNotFoundException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-        }
-       
-    }
 
     public static void main(String... args) {
         CommandLine.run(new JssgApplication(), System.err, args);
