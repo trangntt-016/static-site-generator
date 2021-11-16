5c2ee6f541de5a27207a0f614edbc4be9272a8cd
trangntt-016 <trangntt.016@gmail.com>
2021-11-16 10:31:30 -0500

    fix path of test

--- src/test/java/com/os/jssg/TestUtils.java
+++ src/test/java/com/os/jssg/TestUtils.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 
 public class TestUtils {
     public static String generateRandomInputPath() throws IOException {
-        Path filePath = Files.list(Paths.get("src/main/resources/SherlockHolmesSelectedStories")).findFirst().get();
+        Path filePath = Files.list(Paths.get("./src/main/resources/SherlockHolmesSelectedStories")).findFirst().get();
 
         if (Files.exists(filePath)) {
             return filePath.toString();
@@ -18,7 +18,7 @@ public class TestUtils {
     }
 
     public static String generateConfigPath() throws IOException {
-        Path filePath = Files.list(Paths.get("src/main/resources")).filter(f->f.toString().endsWith(".json")).findFirst().get();
+        Path filePath = Files.list(Paths.get("./src/main/resources")).filter(f->f.toString().endsWith(".json")).findFirst().get();
 
         if (Files.exists(filePath)) {
             return filePath.toString();
